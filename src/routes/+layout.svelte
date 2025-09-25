d175d8456865f0f93444ffd8116bb55ced054e5b
Laurent Curau <github@lqro.slmail.me>
2025-09-25 13:07:21 +0200

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import Navigation from '$lib/Navigation.svelte';
 
 	let { children } = $props();
